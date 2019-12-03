ccffc3434c868f56dde1c5e9d57573154d2a2f98
Owen Brooks <33075324+owen-brooks@users.noreply.github.com>
2019-12-03 14:58:12 -0500

    Create match.js

---
+++ src/utils/match.js
@@ -0,0 +1,25 @@
+var Fuse = require("fuse.js");
+
+exports.match = function(userProfile, listProjects) {
+  var options = {
+    shouldSort: true,
+    threshold: 0.6,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    keys: [
+      {
+        name: "description",
+        weight: 0.3
+      },
+      {
+        name: "skillsNeeded",
+        weight: 0.7
+      }
+    ]
+  };
+  const fuse = new Fuse(listProjects, options); // "list" is the item array
+  return fuse.search(description + skills);
+};
+
+module.exports = utils;
