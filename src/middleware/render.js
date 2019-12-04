831f0fcd75c6476b2034f20fc309576f227dcb85
martyk96 <marty.p.kraus@gmail.com>
2019-12-04 18:08:48 -0500

    render is for .temp files now
    render used .temp files now

--- src/middleware/render.js
+++ src/middleware/render.js
@@ -1,7 +1,14 @@
 var fs = require("fs");
+var navbar = fs.readFileSync('src/templates/navbar.temp','utf8');
+var header = fs.readFileSync('src/templates/header.html','utf8');
+
 function mergeValues(values,content){
+    // scripts and what not needed in the header for the navbar to work
+    content = content.replace("{{header}}",header);
+    // add navbar to each template
+    content = content.replace("{{navbar}}",navbar);
     for(var key in values){
-        content = content.replace("{{"+key+"}}",values[key]);
+        content = content.replace(new RegExp("{{"+key+"}}", "g"),values[key]);
     }
     return content;
 }
@@ -17,9 +24,10 @@ function view(templateName, values, res){
      */
 
     console.log(values);
-    var fileContent = fs.readFileSync('src/views/'+templateName+'.html','utf8');
+    var fileContent = fs.readFileSync('src/templates/'+templateName+'.temp','utf8');
     fileContent = mergeValues(values, fileContent);
     res.write(fileContent);
+    res.end();
 }
 
 module.exports.view = view;
