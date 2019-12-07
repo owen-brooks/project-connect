7b1cabf781d0da2114cd8a59b40117e7880f03e0
martyk96 <marty.p.kraus@gmail.com>
2019-12-06 20:50:00 -0500

    fix for merge conflicts

--- src/middleware/render.js
+++ src/middleware/render.js
@@ -1,25 +1,24 @@
 var fs = require("fs");
-var navbar = fs.readFileSync('src/templates/navbar.temp','utf8');
-var header = fs.readFileSync('src/templates/header.html','utf8');
-// var JSON = require()
+const path = require("path");
 
-function mergeValues(values,content){
-    // scripts and what not needed in the header for the navbar to work
-    content = content.replace("{{header}}",header);
-    // add navbar to each template
-    content = content.replace("{{navbar}}",navbar);
+var navbar = fs.readFileSync(
+  "src" + path.sep + "templates" + path.sep + "navbar.temp",
+  "utf8"
+);
+var header = fs.readFileSync(
+  "src" + path.sep + "templates" + path.sep + "header.html",
+  "utf8"
+);
 
-    // add the raw data
-    var jsonStr = JSON.stringify(values);
-    console.log(jsonStr);
-    content = content.replace("{{rawJSON}}",jsonStr);
-
-    for(var key in values){
-        // content = content.replace(new RegExp("{{"+key+"}}", "g"),values[key]);
-        content = content.replace("{{"+key+"}}",values[key]);
-
-    }
-    return content;
+function mergeValues(values, content) {
+  // scripts and what not needed in the header for the navbar to work
+  content = content.replace("{{header}}", header);
+  // add navbar to each template
+  content = content.replace("{{navbar}}", navbar);
+  for (var key in values) {
+    content = content.replace(new RegExp("{{" + key + "}}", "g"), values[key]);
+  }
+  return content;
 }
 
 function view(templateName, values, res){
@@ -32,7 +31,11 @@ function view(templateName, values, res){
      *      ie: {{title}}
      */
 
-    var fileContent = fs.readFileSync('src/templates/'+templateName+'.temp','utf8');
+    console.log(values);
+    var fileContent = fs.readFileSync(
+      "src" + path.sep + "templates" + path.sep + templateName + ".temp",
+      "utf8"
+    );
     fileContent = mergeValues(values, fileContent);
     res.write(fileContent);
     res.end();
